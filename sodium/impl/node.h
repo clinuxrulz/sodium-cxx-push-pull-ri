4bd55bdcece5d07452275c6fddf0e9aefbeeb861
Clinton Jon Selke <clinuxrulz@gmail.com>
2018-12-31 13:55:55 +1000

    wip translating from rust version

---
+++ sodium/impl/node.h
@@ -0,0 +1,36 @@
+// WIP: Translating from rust version: sodium/impl_/node.rs
+
+#ifndef _SODIUM_IMPL_NODE_H_
+#define _SODIUM_IMPL_NODE_H_
+
+#include "bacon_gc/gc.h"
+#include "functional"
+#include "vector"
+
+namespace sodium::impl {
+
+    typedef struct NodeData;
+
+    typedef struct Node {
+        bacon_gc::Gc<NodeData> data;
+    };
+
+    typedef struct WeakNode {
+        bacon_gc::GcWeak<NodeData> data;
+    };
+
+    typedef struct NodeData {
+        unsigned int id;
+        unsigned int rank;
+        std::function<void()> update;
+        std::vector<bacon_gc::Node> update_dependencies;
+        std::vector<Node> dependencies;
+        std::vector<WeakNode> dependents;
+        std::function<void()> cleanup;
+        std::vector<std::function<void()>> additional_cleanups;
+        // SodiumCtx sodiumCtx;
+    };
+
+}
+
+#endif // _SODIUM_IMPL_NODE_H_
