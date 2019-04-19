76da6ec72c297ee93efc537abb733b266cff5cb2
Clinton Jon Selke <clinuxrulz@gmail.com>
2019-04-19 11:58:30 +1000

    new_node()

--- sodium/impl/node.h
+++ sodium/impl/node.h
@@ -3,55 +3,75 @@
 #ifndef _SODIUM_IMPL_NODE_H_
 #define _SODIUM_IMPL_NODE_H_
 
-#include "bacon_gc/gc.h"
 #include <functional>
 #include <vector>
+#include <memory>
+#include "bacon_gc/gc.h"
+#include "sodium_ctx.h"
 
 namespace sodium::impl {
 
-    struct NodeData;
-
-    struct Node {
-        bacon_gc::Gc<NodeData> data;
-    };
-
-    struct WeakNode {
-        bacon_gc::GcWeak<NodeData> data;
-    };
-
-    struct NodeData {
-        unsigned int id;
-        unsigned int rank;
-        std::function<bool()> update;
-        std::vector<bacon_gc::Node> update_dependencies;
-        std::vector<Node> dependencies;
-        std::vector<WeakNode> dependents;
-        std::function<void()> cleanup;
-        std::vector<std::function<void()>> additional_cleanups;
-    };
-}
-
-namespace std {
-    template<>
-    struct hash<sodium::impl::Node> {
-        size_t operator()(const sodium::impl::Node& node) const {
-            return node.data->id;
+    template <typename UPDATE, typename CLEANUP>
+    static Node node_new(
+        UPDATE update,
+        std::vector<bacon_gc::Node*> update_dependencies,
+        std::vector<Node> dependencies,
+        CLEANUP cleanup,
+        std::string desc
+    ) {
+        int id = with_sodium_ctx<int>([](SodiumCtx sodium_ctx) {
+            sodium_ctx.inc_node_count();
+            return sodium_ctx.new_id();
+        });
+        int rank = 0;
+        for (auto it = dependencies.begin(); it != dependencies.end(); ++it) {
+            auto& dependency = *it;
+            if (rank <= dependency.data->rank) {
+                rank = dependency.data->rank + 1;
+            }
         }
-    };
+        auto update2 = [=]() {
+            with_sodium_ctx_void([](SodiumCtx sodium_ctx) {
+                sodium_ctx.inc_callback_depth();
+            });
+            auto result = update();
+            with_sodium_ctx_void([](SodiumCtx sodium_ctx) {
+                sodium_ctx.dec_callback_depth();
+            });
+            return result;
+        };
+        std::shared_ptr<NodeData> _self;
+        auto cleanup2 = [=]() {
+            cleanup();
+            for (auto it = dependencies.begin(); it != dependencies.end(); ++it) {
+                auto& dependency = *it;
+                for (auto it2 = dependency.data->dependents.begin(); it2 != dependency.data->dependents.end(); ++it2) {
+                    auto& dependent = *it2;
+                    auto dependent2 = dependent.data.lock();
+                    if (dependent2) {
+                        if (dependent2->id == id) {
+                            dependency.data->dependents.erase(it2);
+                            break;
+                        }
+                    }
+                }
+            }
+            for (auto it = _self->additional_cleanups.begin(); it != _self->additional_cleanups.end(); ++it) {
+                auto& cleanup = *it;
+                cleanup();
+            }
+        };
+        NodeData* node_data = new NodeData;
+        node_data->id = id;
+        node_data->rank = rank;
+        node_data->update = update2;
+        node_data->update_dependencies = update_dependencies;
+        node_data->dependencies = dependencies;
+        node_data->cleanup = cleanup2;
+        _self = node_data;
+        return { data: bacon_gc::Gc<NodeData>(node_data) };
+    }
 
-    template<>
-    struct equal_to<sodium::impl::Node> {
-        bool operator()(const sodium::impl::Node& lhs, const sodium::impl::Node& rhs) const {
-            return lhs.data->id == rhs.data->id;
-        }
-    };
-
-    template<>
-    struct less<sodium::impl::Node> {
-        bool operator()(const sodium::impl::Node& lhs, const sodium::impl::Node& rhs) const {
-            return lhs.data->rank < rhs.data->rank;
-        }
-    };
 }
 
 #endif // _SODIUM_IMPL_NODE_H_
--- sodium/impl/sodium_ctx.h
+++ sodium/impl/sodium_ctx.h
@@ -13,6 +13,55 @@
 
 namespace sodium::impl {
 
+    struct NodeData;
+
+    struct Node {
+        bacon_gc::Gc<NodeData> data;
+    };
+
+    struct WeakNode {
+        bacon_gc::GcWeak<NodeData> data;
+    };
+
+    struct NodeData {
+        unsigned int id;
+        unsigned int rank;
+        std::function<bool()> update;
+        std::vector<bacon_gc::Node> update_dependencies;
+        std::vector<Node> dependencies;
+        std::vector<WeakNode> dependents;
+        std::function<void()> cleanup;
+        std::vector<std::function<void()>> additional_cleanups;
+    };
+
+}
+
+namespace std {
+
+    template<>
+    struct hash<sodium::impl::Node> {
+        size_t operator()(const sodium::impl::Node& node) const {
+            return node.data->id;
+        }
+    };
+
+    template<>
+    struct equal_to<sodium::impl::Node> {
+        bool operator()(const sodium::impl::Node& lhs, const sodium::impl::Node& rhs) const {
+            return lhs.data->id == rhs.data->id;
+        }
+    };
+
+    template<>
+    struct less<sodium::impl::Node> {
+        bool operator()(const sodium::impl::Node& lhs, const sodium::impl::Node& rhs) const {
+            return lhs.data->rank < rhs.data->rank;
+        }
+    };
+
+}
+
+namespace sodium::impl {
 
     struct SodiumCtx {
         unsigned int _next_id = 0;
