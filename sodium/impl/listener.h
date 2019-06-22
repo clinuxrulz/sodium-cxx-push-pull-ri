e88a7311e604007aac73c7070f3ed68ba7b88303
Clinton Jon Selke <clinuxrulz@gmail.com>
2019-06-22 11:19:24 +1000

    trace definition for listener

--- sodium/impl/listener.h
+++ sodium/impl/listener.h
@@ -1,6 +1,7 @@
 #ifndef _SODIUM_IMPL_LISTENER_H_
 #define _SODIUM_IMPL_LISTENER_H_
 
+#include "bacon_gc/gc.h"
 #include "sodium/impl/node.h"
 
 namespace sodium::impl {
@@ -26,5 +27,24 @@ namespace sodium::impl {
         }
     };
 };
+
+namespace bacon_gc {
+
+    template<>
+    struct Trace<sodium::impl::Listener> {
+        template <typename F>
+        static void trace(const sodium::impl::Listener& a, F&& k) {
+            Trace<bacon_gc::Gc<sodium::impl::ListenerData>>::trace(a.data, k);
+        }
+    };
+
+    template<>
+    struct Trace<sodium::impl::ListenerData> {
+        template <typename F>
+        static void trace(const sodium::impl::ListenerData& a, F&& k) {
+            Trace<sodium::impl::Node>::trace(a.node, k);
+        }
+    };
+}
 
 #endif // _SODIUM_IMPL_LISTENER_H_
